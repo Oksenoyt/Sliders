9fde181c8cfcf601aba7e8fea70902c2c1db2b47
Arseniy Oksenoyt <a.oksenoyt@gmail.com>
2023-06-17 21:07:10 -0700

    Initial Commit

---
+++ Sliders/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sliders
+//
+//  Created by Arseniy Oksenoyt on 6/17/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
