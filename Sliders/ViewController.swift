4b52abdc4a707e235f4dfb60877c990df09c0e06
Arseniy Oksenoyt <a.oksenoyt@gmail.com>
2023-06-17 23:18:55 -0700

    Final variant

--- Sliders/ViewController.swift
+++ Sliders/ViewController.swift
@@ -9,11 +9,61 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        slidersSetup()
+        labelSetup()
+        colorViewSetup()
     }
 
-
+    @IBAction func changingColorSliders() {
+        redLabel.text = String(format: "%.2f", redSlider.value)
+        greenLabel.text = String(format: "%.2f", greenSlider.value)
+        blueLabel.text = String(format: "%.2f", blueSlider.value)
+        
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+    }
+    
+    private func slidersSetup() {
+        redSlider.value = 0.1
+        greenSlider.value = 0.3
+        blueSlider.value = 0.6
+        
+        redSlider.minimumTrackTintColor = .red
+        greenSlider.minimumTrackTintColor = .green
+        blueSlider.minimumTrackTintColor = .blue
+    }
+    
+    private func labelSetup() {
+        redLabel.text = String(format: "%.2f", redSlider.value)
+        greenLabel.text = String(format: "%.2f", greenSlider.value)
+        blueLabel.text = String(format: "%.2f", blueSlider.value)
+    }
+    
+    private func colorViewSetup() {
+        colorView.layer.cornerRadius = 20
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+    }
+    
 }
 
